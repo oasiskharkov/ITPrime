f8b75183628d0402117748ff53bc248e56c658b6
Aleksei Klopkov <kasystems@mail.ru>
2021-12-15 16:40:49 +0300

    Добавьте файлы проекта.

---
+++ ITPrime.cpp
@@ -0,0 +1,21 @@
+﻿#include "beautifulWordCounter.h"
+
+int main()
+{
+    try
+    {
+        BeautifulWordCounter bwc;
+        std::string number;
+        while (std::cin >> number)
+        {
+            bwc.checkNumber(number);
+        }
+        std::cout << "Beautiful words count: " << bwc.count() << std::endl;
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+
+    return 0;
+}
